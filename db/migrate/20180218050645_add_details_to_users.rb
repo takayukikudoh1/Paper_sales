f21d1dce12a40cff5b35bc1802771f9494997c81
tech-camp <>
2018-04-15 15:11:55 +0900

    migration file

--- db/migrate/20180218050645_add_details_to_users.rb
+++ db/migrate/20180218050645_add_details_to_users.rb
@@ -1,5 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration
   def change
+    add_column :users, :company, :string
     add_column :users, :name, :string
     add_column :users, :tel, :integer
     add_column :users, :fax, :integer
