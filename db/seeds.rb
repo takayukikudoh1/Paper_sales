3f239f38d6818f73b3ebe66d19582b37c73b3cca
tech-camp <p08013421479@icloud.com>
2018-04-22 17:42:47 +0900

    message

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require "csv"
 
-products_csv = CSV.readlines("db/products.csv")
-products_csv.shift
-products_csv.each do |row|
-  Product.create(product_grade: row[1], supplier: row[2], product_name: row[3], basis_weight: row[4], width: row[5], length: row[6], renryo: row[7], inventory: row[8], price: row[10], text: row[11])
+# products_csv = CSV.readlines("db/products.csv")
+# products_csv.shift
+# products_csv.each do |row|
+#   Product.create(product_grade: row[1], supplier: row[2], product_name: row[3], basis_weight: row[4], width: row[5], length: row[6], renryo: row[7], inventory: row[8], price: row[10], text: row[11])
+Admin.create(name: 'takayuki', email: 'p08013421479@icloud.com', password: 'pp051050')
 end
