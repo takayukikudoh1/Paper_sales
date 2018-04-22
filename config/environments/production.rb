75dcf3d8731140fb080f9dbf5c85a3d9ce2fe1d6
tech-camp <>
2018-04-22 16:09:38 +0900

    message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://papersales.herokuapp.com', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "towakud0512@gmail.com", #ご自身のgmailアドレス
+    :password => "pp051050", #ご自身のgmailアドレスのパスワード
+    :authentication => 'login'
 end
